f4362798359a1be6bd6065ffbcf86d49e57eb026
mdegrauxTP <89838999+mdegrauxTP@users.noreply.github.com>
2023-03-03 14:21:19 +0100

    Create RefApplicationParametersValidator.cs

---
+++ TPSecurity.Application/Core/SecuWeb/RefApplicationCore/Validations/RefApplicationParametersValidator.cs
@@ -0,0 +1,13 @@
+﻿using TPSecurity.Application.Common.Validations;
+using TPSecurity.Application.Core.SecuWeb.RefApplicationCore.Queries.GetAllRefApplication;
+
+namespace TPSecurity.Application.Core.SecuWeb.RefApplicationCore.Validations
+{
+    public class RefApplicationParametersValidator : QueryParametersValidator<RefApplicationParameters>
+    {
+        public RefApplicationParametersValidator()
+        {
+
+        }
+    }
+}
