6c2cff0e501e5311a91c7ad66cc5104916d11b98
sleonard <s.leonard@thomas-piron.eu>
2023-03-07 11:04:44 +0100

    Rename dossier

--- TPSecurity.Application/Core/SecuWeb/AccesApplicationCore/Queries/GetAllAccesGroupe/GetAllAccesApplicationQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using ErrorOr;
-using MediatR;
-using TPSecurity.Application.Common;
-using TPSecurity.Application.Core.SecuWeb.AccesApplicationCore.Common;
-
-namespace TPSecurity.Application.Core.SecuWeb.AccesApplicationCore.Queries.GetAllAccesApplication
-{
-    public record GetAllAccesApplicationQuery(AccesApplicationParameters queryParameters) : IRequest<ErrorOr<PagedList<AccesApplicationResult>>>;
-}
--- TPSecurity.Application/Core/SecuWeb/AccesApplicationCore/Queries/GetByIdAccesGroupe/GetByIdAccesApplicationQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using ErrorOr;
-using MediatR;
-using TPSecurity.Application.Core.SecuWeb.AccesApplicationCore.Common;
-
-namespace TPSecurity.Application.Core.SecuWeb.AccesApplicationCore.Queries.GetByIdAccesApplication
-{
-    public record GetByIdAccesApplicationQuery(int Id) : IRequest<ErrorOr<AccesApplicationResult>>;
-}
